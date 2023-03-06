80aba5897c23c3452af61d09f51fb7efebb220c2
Steven <i507678>
2023-03-06 18:37:00 +0100

    fix

---
+++ Website/WebApplication/WebApplication1/Pages/Register.cshtml.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication1.Modals;
+
+namespace WebApplication1.Pages
+{
+    public class RegisterModel : PageModel
+    {
+        [BindProperty]
+        public User User { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
