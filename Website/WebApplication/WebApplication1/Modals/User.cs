d556f9da44ba57c648138a6c0f5d0fcc87b7f1e6
Steven <i507678>
2023-03-06 01:12:07 +0100

    Week 3 submission

---
+++ Website/WebApplication/WebApplication1/Modals/User.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication1.Modals
+{
+	public class User
+	{
+		public string Email { get; set; }
+		public string Password { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Gender { get; set; }
+		public string Contact { get; set; }
+	}
+}
