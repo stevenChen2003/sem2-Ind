928383f3dffcb7b358f89ab6b04e59ccb2f3b705
Steven <i507678>
2023-03-31 13:30:33 +0200

    Encrypt password done

--- BookedProject/Booked.Logic/Services/UserManager.cs
+++ BookedProject/Booked.Logic/Services/UserManager.cs
@@ -3,6 +3,7 @@ using Booked.Infrastructure.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -25,17 +26,41 @@ namespace Booked.Logic.Services
 			}
 			else
 			{
+				user.Password = EncryptPassword(user.Password);
                 userRepo.AddUser(user);
 				return true;
             }
 		}
 
-		//Need to make the hash method
-		public string HashPassword(string password)
+		public string EncryptPassword(string password)
 		{
-			return "";
+			byte[] salt = GenerateSalt(16);
+            var hashedPassword = new Rfc2898DeriveBytes(password, salt, 10_000).GetBytes(20);
+
+            byte[] hashBytes = new byte[36];
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hashedPassword, 0, hashBytes, 16, 20);
+            string hashedPasswordBase64 = Convert.ToBase64String(hashBytes);
+
+            return hashedPasswordBase64;
 		}
 
+        public byte[] GenerateSalt(int saltLength)
+        {
+            byte[] salt = new byte[saltLength];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+            return salt;
+        }
+
+
+		//Need to finish this method
+		public bool Authentication(string password)
+		{
+			return true;
+		}
 
     }
 }
