ed2f5018e8ed6847369c48ea13e94b51e9f2cdc1
Steven <i507678>
2023-04-03 18:51:32 +0200

    User information page done

--- BookedProject/BookedWebApp/Pages/UserInfo.cshtml.cs
+++ BookedProject/BookedWebApp/Pages/UserInfo.cshtml.cs
@@ -4,14 +4,27 @@ using Microsoft.AspNetCore.Authorization;
 using System.Security.Claims;
 using Booked.Domain.Domain;
 using Booked.Logic.Services;
+using Microsoft.AspNetCore.Identity;
 
 namespace BookedWebApp.Pages
 {
     [Authorize]
     public class UserInfoModel : PageModel
     {
-        public void OnGet()
+		private readonly UserManager userManager;
+
+		public UserInfoModel()
         {
+			userManager = new UserManager();
+		}
+
+		[BindProperty]
+		public User user { get; set; }
+
+		public void OnGet()
+        {
+            string userEmail = User.Identity.Name;
+            user = userManager.GetUser(userEmail);
         }
     }
 }
