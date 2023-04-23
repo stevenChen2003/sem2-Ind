d3c4c236fe24932fd70fedecf76f03c4580b8986
Steven <i507678>
2023-04-24 01:37:54 +0200

    interface made need to just make the methods

---
+++ BookedProject/Booked.Infrastructure/Interfaces/IFlightRepo.cs
@@ -0,0 +1,18 @@
+﻿using Booked.Domain.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booked.Infrastructure.Interfaces
+{
+    public interface IFlightRepo
+    {
+        Flight GetFlightByID(int id);
+        IEnumerable<Flight> GetAllFlight();
+        void AddFlight(Flight flight);
+        void UpdateHotel(Flight flight);
+        void RemoveFlightByID(int id);
+    }
+}
--- BookedProject/Booked.Infrastructure/Repositories/FlightRepository.cs
+++ BookedProject/Booked.Infrastructure/Repositories/FlightRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Booked.Domain.Domain;
+using Booked.Infrastructure.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +8,33 @@ using System.Threading.Tasks;
 
 namespace Booked.Infrastructure.Repositories
 {
-    public class FlightRepository
+    public class FlightRepository : IFlightRepo
     {
         private const string CONNECTION_STRING = @"Server=mssqlstud.fhict.local;Database=dbi507678_booked;User Id=dbi507678_booked;Password=booked789;";
 
+        public Flight GetFlightByID(int id)
+        {
+            throw new NotImplementedException();
+        }
 
+        public IEnumerable<Flight> GetAllFlight()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddFlight(Flight flight)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateHotel(Flight flight)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveFlightByID(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
