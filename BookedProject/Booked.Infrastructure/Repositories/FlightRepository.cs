45048e8a5eccca19549bcfb1829e20e7d7c76e8e
Steven <i507678>
2023-04-24 02:30:10 +0200

    get fligth method is done

--- BookedProject/Booked.Infrastructure/Repositories/FlightRepository.cs
+++ BookedProject/Booked.Infrastructure/Repositories/FlightRepository.cs
@@ -1,7 +1,9 @@
 ﻿using Booked.Domain.Domain;
+using Booked.Domain.Domain.Enum;
 using Booked.Infrastructure.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,7 +16,38 @@ namespace Booked.Infrastructure.Repositories
 
         public Flight GetFlightByID(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(CONNECTION_STRING))
+                {
+                    string query = @"SELECT * FROM Flights WHERE FlightId = @FlightId; ";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+
+                    conn.Open();
+                    cmd.Parameters.AddWithValue("@FlightId", id);
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    if (dr.Read())
+                    {
+                        Flight flightDetails = new Flight(Convert.ToInt32(dr["FlightId"]),
+                                                  dr["Airline"].ToString(),
+                                                  dr["DepartureAirport"].ToString(),
+                                                  dr["DepartureCountry"].ToString(),
+                                                  dr["ArrivalAirport"].ToString(),
+                                                  dr["ArrivalCountry"].ToString(),
+                                                  Convert.ToDecimal(dr["Price"]),
+                                                  (Seats)Enum.Parse(typeof(Seats), dr["SeatType"].ToString()),
+                                                  Convert.ToInt32(dr["NumberOfSeats"]),
+                                                  Convert.ToDecimal(dr["ExtraBaggagePrice"]));
+                        return flightDetails;
+                    }
+                    conn.Close();
+                }
+                return null;
+            }
+            catch (Exception)
+            {
+                throw new Exception("Flight not found");
+            }
         }
 
         public IEnumerable<Flight> GetAllFlight()
