b92e53445276dfdca50a4173d2fbf62804214ad1
Steven <i507678>
2023-04-24 23:14:09 +0200

    clean up and flight manager is almost done just need to do the validation.

--- BookedProject/Booked.Logic/Services/FlightManager.cs
+++ BookedProject/Booked.Logic/Services/FlightManager.cs
@@ -1,4 +1,5 @@
 ﻿using Booked.Domain.Domain;
+using Booked.Logic.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,8 +10,59 @@ namespace Booked.Logic.Services
 {
     public class FlightManager
     {
-        public FlightManager() { }
+        private IFlightRepo flightRepo;
 
-        public Flight GetFlight() { return null; }
+        public FlightManager(IFlightRepo repo)
+        {
+            flightRepo = repo;
+        }
+
+        public Flight GetFlight(int id)
+        {
+            return flightRepo.GetFlightByID(id);
+        }
+
+        public IEnumerable<Flight> GetAllFlight()
+        {
+            return flightRepo.GetAllFlight();
+        }
+
+        public void AddFlight(Flight flight)
+        {
+            try
+            {
+                flightRepo.AddFlight(flight);
+            }
+            catch(Exception)
+            {
+                throw new Exception("Adding not succesfull");
+            }
+        }
+
+
+        public void UpdateFlight(Flight flight)
+        {
+            try
+            {
+                flightRepo.UpdateHotel(flight);
+            }
+            catch(Exception)
+            {
+                throw new Exception("Cannot update flight information");
+            }
+        }
+
+        public void RemoveFlight(int id)
+        {
+            try
+            {
+                flightRepo.RemoveFlightByID(id);
+            }
+            catch (Exception)
+            {
+                throw new Exception("Cannot remove hotel");
+            }
+        }
+
     }
 }
