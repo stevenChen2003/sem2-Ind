9e822b17e500c624c092a863bbcd08d96baae108
Steven <i507678>
2023-05-02 01:21:32 +0200

    .

--- BookedProject/BookedWebApp/Pages/Users/Edit.cshtml.cs
+++ BookedProject/BookedWebApp/Pages/Users/Edit.cshtml.cs
@@ -19,6 +19,8 @@ namespace BookedWebApp.Pages.Users
             userManager = new UserManager(new UserRepository());
         }
 
+
+        //Change to DTO's later on in life
         [BindProperty]
         public User user { get; set; }
 
