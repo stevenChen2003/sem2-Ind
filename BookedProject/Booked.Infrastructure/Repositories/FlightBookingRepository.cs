06f6aed4bdcdad90255f138487763e6ef4cf5604
Steven <i507678>
2023-05-08 22:49:34 +0200

    clean up

---
+++ BookedProject/Booked.Infrastructure/Repositories/FlightBookingRepository.cs
@@ -0,0 +1,45 @@
+﻿using Booked.Domain.Domain;
+using Booked.Logic.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booked.Infrastructure.Repositories
+{
+	public class FlightBookingRepository : IFlightBookingRepo
+	{
+		private const string CONNECTION_STRING = @"Server=mssqlstud.fhict.local;Database=dbi507678_booked;User Id=dbi507678_booked;Password=booked789;";
+
+		public void AddBooking(FlightBooking booking)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<Booking> GetAllBooking()
+		{
+			throw new NotImplementedException();
+		}
+
+		public FlightBooking GetBookingById(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<Booking> GetBookingByUserId(int userId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool RemoveBooking(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool UpdateBooking(FlightBooking booking)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
--- BookedProject/Booked.Logic/Interfaces/IFlightBookingRepo.cs
+++ BookedProject/Booked.Logic/Interfaces/IFlightBookingRepo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Booked.Domain.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,11 @@ namespace Booked.Logic.Interfaces
 {
 	public interface IFlightBookingRepo
 	{
+		IEnumerable<Booking> GetAllBooking();
+		IEnumerable<Booking> GetBookingByUserId(int userId);
+		FlightBooking GetBookingById(int id);
+		void AddBooking(FlightBooking booking);
+		bool UpdateBooking(FlightBooking booking);
+		bool RemoveBooking(int id);
 	}
 }
