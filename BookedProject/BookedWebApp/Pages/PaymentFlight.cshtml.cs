6f2deedc2ea6794bb15619cd6498694f810cd5ad
Steven <i507678>
2023-05-11 13:02:59 +0200

    .

---
+++ BookedProject/BookedWebApp/Pages/PaymentFlight.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookedWebApp.Pages
+{
+    public class PaymentFlightModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
