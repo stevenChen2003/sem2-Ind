cf41d9785507b25d2888f4014c8ecce1c5648b3b
Steven <i507678>
2023-05-18 18:57:34 +0200

    .

---
+++ BookedProject/BookedWebApp/Pages/Users/PaymentDetails.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookedWebApp.Pages.Users
+{
+    public class PaymentDetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
