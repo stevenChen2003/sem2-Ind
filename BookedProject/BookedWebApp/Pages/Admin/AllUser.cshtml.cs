70c6a020386f23b80fecc6a438b8fa759a8b2197
Steven <i507678>
2023-06-07 11:28:36 +0200

    clean up

---
+++ BookedProject/BookedWebApp/Pages/Admin/AllUser.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookedWebApp.Pages.Admin
+{
+    public class AllUserModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ BookedProject/BookedWebApp/Pages/Admin/Bookings.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookedWebApp.Pages.Admin
+{
+    public class BookingsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
