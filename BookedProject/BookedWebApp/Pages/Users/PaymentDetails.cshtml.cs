6699030223b9b2104d57491f1c588e7d39cdd780
Steven <i507678>
2023-06-07 15:48:45 +0200

    clean up

--- BookedProject/BookedWebApp/Pages/Users/PaymentDetails.cshtml.cs
+++ BookedProject/BookedWebApp/Pages/Users/PaymentDetails.cshtml.cs
@@ -1,4 +1,5 @@
 using Booked.Domain.Domain;
+using Booked.Domain.Domain.Enum;
 using Booked.Logic.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -12,31 +13,50 @@ namespace BookedWebApp.Pages.Users
         private readonly BookingManager bookingManager;
         private readonly HotelManager hotelManager;
         private readonly FlightManager flightManager;
+        private readonly UserManager userManager;
 
-        public PaymentDetailsModel(BookingManager bookingManager, HotelManager hotelManager, FlightManager flightManager)
+        public PaymentDetailsModel(BookingManager bookingManager, HotelManager hotelManager, FlightManager flightManager, UserManager userManager)
         {
             this.bookingManager = bookingManager;
             this.hotelManager = hotelManager;
             this.flightManager = flightManager;
+            this.userManager = userManager;
         }
 
         public FlightBooking FlightBooking { get; set; }
         public HotelBooking HotelBooking { get; set; }
 
-        public void OnGet(int id)
+        public IActionResult OnGet(int id)
         {
+            User user = userManager.GetUser(User.Identity.Name);
             if (bookingManager.GetBookingByid(id) is FlightBooking)
 			{
                 FlightBooking  = (FlightBooking)bookingManager.GetBookingByid(id);
                 FlightBooking.Flight = flightManager.GetFlight(FlightBooking.Flight.FlightId);
-			}
+                if (user.UserType == UserType.Admin || FlightBooking.User.UserId == user.UserId)
+                {
+                    return Page();
+                }
+                else
+                {
+                    return Redirect("/AccessDenied");
+                }
+            }
             else if (bookingManager.GetBookingByid(id) is HotelBooking)
             {
 				HotelBooking = (HotelBooking)bookingManager.GetBookingByid(id);
                 HotelBooking.Hotel = hotelManager.GetHotel(HotelBooking.Hotel.HotelId);
-			}
+                if (user.UserType == UserType.Admin || FlightBooking.User.UserId == user.UserId)
+                {
+                    return Page();
+                }
+                else
+                {
+                    return Redirect("/AccessDenied");
+                }
+            }
+            return Page();
 
-
-		}
+        }
     }
 }
